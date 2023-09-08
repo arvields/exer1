b08897b164791250ec70861f369d7845c762356c
Arviel Delos Santos <133615240+arvields@users.noreply.github.com>
2023-09-08 10:46:17 +0800

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Your code here, including the displaySum and displayDiff functions
+  function displaySum() {
+    let firstNum = Number(document.getElementById('firstNum').innerHTML);
+    let secondNum = Number(document.getElementById('secondNum').innerHTML);
+    let total = firstNum + secondNum;
+    document.getElementById("sumanswer").innerHTML = ${firstNum} + ${secondNum}, equals to ${total};
+  }
 
   function displayDiff() {
     let firstNum = Number(document.getElementById('firstNum').innerHTML);
@@ -8,12 +15,14 @@ document.addEventListener("DOMContentLoaded", function () {
   }
   
   function displayMult() {
+  console.log("Multiply button clicked"); // Add this line to check if the function is called
   let firstNum = Number(document.getElementById('firstNum').innerHTML);
   let secondNum = Number(document.getElementById('secondNum').innerHTML);
   let product = firstNum * secondNum;
   document.getElementById("productanswer").innerHTML = ${firstNum} * ${secondNum}, equals to ${product};
-  }
+}
 
   document.getElementById('MultButton').addEventListener("click", displayMult);
+  document.getElementById('SumButton').addEventListener("click", displaySum);
   document.getElementById('DiffButton').addEventListener("click", displayDiff);
 });
